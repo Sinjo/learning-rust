c06555532c1d055fdf2866fc409c006fa21153ce
Chris Sinjakli <chris@sinjakli.co.uk>
2017-05-07 23:29:01 +0100

    Inital commit

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    for i in 1..101 {
+        if i % 3 == 0 {
+            print!("Fizz");
+        }
+        if i % 5 == 0 {
+            print!("Buzz");
+        }
+        if i % 3 != 0 && i % 5 != 0 {
+            print!("{}", i);
+        }
+        print!("\n");
+    }
+}
