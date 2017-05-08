757405958acc632f92d12ad1d398fa27b2662c5c
Chris Sinjakli <chris@sinjakli.co.uk>
2017-05-08 22:35:44 +0100

    Sketch out rough flow in comment

--- simple_crawler/src/main.rs
+++ simple_crawler/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    // parse domain argument
+    // add argument to queue
+    // start work method
+    //   dequeue a link, add to visited set
+    //   extract links and assets - add to hash
+    //   add any links not in visited set to queue
+    //   repeat until queue is empty
+    // serialise to json
     println!("Hello, world!");
 }
