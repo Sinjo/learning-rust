6e927c3ff036e0824a0c4085db6f1fc6dd87622e
Chris Sinjakli <chris@sinjakli.co.uk>
2018-12-13 16:08:42 +0000

    Add command line parsing

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("crawler").
+        version("0.0.1").
+        author("Chris Sinjakli <chris@sinjakli.co.uk>").
+        about("A single-threaded, single-domain web crawler").
+        arg(Arg::with_name("start-url").
+            short("s").
+            long("start-url").
+            value_name("URL").
+            help("The URL of the page to start crawling from").
+            takes_value(true).
+            required(true)
+            ).
+        get_matches();
+
+    let start_url = matches.value_of("start-url").unwrap();
+
+    println!("Let's crawl {}!", start_url);
 }
